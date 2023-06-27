978762e145d90035cdde8af065583d75c78f2165
Aaliyah Merchant <meraliy@icloud.com>
2023-06-27 01:54:08 +0100

    Fixed client folder

---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose, legacy_createStore } from 'redux';
+//import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+import App from './App';
+
+const store = legacy_createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+
