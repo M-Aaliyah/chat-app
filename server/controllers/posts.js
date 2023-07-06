912b35b83868a896a72edc9680860c4f25e4d27e
Aaliyah Merchant <meraliy@icloud.com>
2023-07-07 00:03:40 +0100

    Implemented functionality to update posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,6 @@
 // All the handlers for the routes
+import express from 'express';
+import mongoose from 'mongoose';
 
 import PostMessage from '../models/postMessage.js';
 
@@ -25,4 +27,15 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({message: error.message});
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+    res.json(updatedPost);
 }
